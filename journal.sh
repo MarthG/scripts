9fc05af3aac483b4c143bd5045277fc8632311e3
Carlos G <cgcarlos444@gmail.com>
2020-07-22 22:15:28 +0200

    Fixed help message

--- journal.sh
+++ journal.sh
@@ -2,6 +2,7 @@
 jndir="/mnt/b/journal"
 jndatefmt="%d-%b-%Y"
 jnfmt="$(date +$jndatefmt)"
+jntoday="$jndir/$jnfmt"
 editor="/usr/bin/vi"
 
 
@@ -28,7 +29,12 @@ listFiles() {
 }
 
 editToday() {
-	jntoday="$jndir/$jnfmt"
+	if ! [[ -z $2 ]] && [[ -e $jndir/$2 ]]; then
+		$editor $jndir/$2
+		if [[ $? == 1 ]]; then
+			echo "There was an error opening [$jndir/$2]."
+		fi
+	fi
 	if ! [[ -e $jntoday ]]; then
 		echo "Today's journal doesn't exist. Creating it."
 		touch $jntoday
@@ -38,13 +44,25 @@ editToday() {
 }
 
 doHelp() {
-	echo "stub. help is \'-l\' to list files, nothing or \'-e\' to edit today's file."
+	echo "stub. help is '-l' to list files, nothing or '-e' to edit today's file, '-c [filename]' to cat it, and '-h', '-?','--help' or nothing to show this message."
+}
+
+catFile() {
+	if ! [[ -z $2 ]] && [[ -e $jndir/$2 ]]; then
+		cat $jndir/$2
+	fi
+	if ! [[ -e $jntoday ]]; then
+		echo "The file [$jntoday] doesn't exist."
+		exit -1;
+	fi
+	cat $jntoday
 }
 
 case $1 in
 		-l | ls | l | list) listFiles;;
 		-h | -? | --help | --?) doHelp;;
 		-e | edit | e | --edit) editToday;;
+		-c | cat | c | print) catFile;;
 		*) doHelp;;
 esac
 
